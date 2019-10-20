65239b06c0a7e518fdcc8a3c1d090aa5804fa7f0
VolokzhaninVadim <volokzhanin@yandex.ru>
2019-10-20 23:28:26 +1000

    Координаты получаю только через yandex

--- scripts/geocoder_loader.py
+++ scripts/geocoder_loader.py
@@ -111,88 +111,50 @@ class geocoder:
                 continue
                 driver.close()
 
-    def osm_coordinates(self, address, timeout = 10) -> list():
-        """
-        Функция для возвращения долготы и широты с API OSM.
-        Параметры: address - адрес в строке, headers - заголовки, proxies- прокси сервера, timeout - timeout.
-        Выход - список из широты и долготы.
-        """
-        url = 'https://nominatim.openstreetmap.org/search/?'
-        params = {'q': address, 'format': 'json', 'addressdetails': '1', 'limit' : '1'}
-        while True:
-            try:
-                proxies = next(self.proxy_cycle)
-                headers = ua.Chrome
-                result = requests.get(
-                    url = url,
-                    params = params,
-                    headers = {'User-Agent' : headers},
-                    proxies = {'https' : 'https://' + proxies},
-                    timeout = timeout
-                )
-                geojson = result.json()
-                if len(geojson) > 0:
-                    latitude = float(geojson[0]['lat']) if geojson[0]['class'] == 'building' else None
-                    longitude = float(geojson[0]['lon']) if geojson[0]['class'] == 'building' else None
-
-                else:
-                    latitude = None
-                    longitude = None
-                break
-            except (ConnectTimeout, ConnectionError, ReadTimeout) as e:
-                continue
-        return [latitude, longitude]
-
     def coordinates(self, address, city_latitude = 43.115536, city_longitude = 131.885485) -> pd.DataFrame():
         """
         Функция для получения широты и долготы адреса с yandex карт.
         Вход: текст адреса, широта города, долгота города.
         Выход: таблица: адрес, широта и долгота.
         """
-        # Для рааботы с API OSM убираем призак дома (д.)
-        address_prepared = address.replace(' д. ', ' ')
-        latitude, longitude = self.osm_coordinates(address)
-        if latitude is None:
-            while True:
-                try:
-                    proxies = next(self.proxy_cycle)
-                    headers = ua.Chrome
-                    url = 'https://yandex.ru/maps/75/vladivostok/?text={text}'.format(text = urllib.parse.quote(address))
-                    chrome_options = webdriver.ChromeOptions()
-                    # Вставляем прокси
-                    chrome_options.add_argument('--proxy-server={proxy}'.format(proxy = proxies))
-                    # Вставляем user agent
-                    chrome_options.add_argument("user-agent={user_agent}".format(user_agent = headers))
-                    #  Запускаем без графического драйвера
-                    chrome_options.add_argument('headless')
-                    driver = webdriver.Chrome(options = chrome_options)
-                    # Установим time out
-                    driver.implicitly_wait(10)
-                    driver.get(url)
-                    # Ждем загрузки левой панели
-                    wait(driver, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR, '._view_full')))
-                    # Ищем плашку с координатами
-                    driver.find_element(By.CSS_SELECTOR, '.card-title-view')
-                    page_source = driver.page_source
-                    bsObj = BeautifulSoup(page_source, 'html5lib')
-                    coordinats_list = bsObj.find_all('div', {'class' : 'clipboard__action-wrapper _inline'})
-                    # Ищем координтаы
-                    if len(coordinats_list) > 0:
-                        latitude, longitude = re.findall(r'\d{1,3}.\d{1,10}', coordinats_list[0].text)
-                    else:
-                        latitude, longitude = None, None
-                    if latitude == city_latitude and longitude == city_longitude:
-                        latitude, longitude = None, None
-                    else:
-                        latitude = latitude
-                        longitude = longitude
-                    return pd.DataFrame({'address' : address, 'latitude' : [latitude], 'longitude' : [longitude], 'geom' : [None], 'address_prepared' : [address_prepared]})
-                    break
-                except NoSuchElementException:
-                    return pd.DataFrame({'address': address, 'latitude': [None], 'longitude': [None], 'geom': [None], 'address_prepared': [address_prepared]})
-                except TimeoutException:
-                    continue
-                finally:
-                    driver.close()
-        else:
-            return pd.DataFrame({'address' : address, 'latitude' : [latitude], 'longitude' : [longitude], 'geom' : [None], 'address_prepared' : [address_prepared]})
+        while True:
+            try:
+                proxies = next(self.proxy_cycle)
+                headers = ua.Chrome
+                url = 'https://yandex.ru/maps/75/vladivostok/?text={text}'.format(text = urllib.parse.quote(address))
+                chrome_options = webdriver.ChromeOptions()
+                # Вставляем прокси
+                chrome_options.add_argument('--proxy-server={proxy}'.format(proxy = proxies))
+                # Вставляем user agent
+                chrome_options.add_argument("user-agent={user_agent}".format(user_agent = headers))
+                #  Запускаем без графического драйвера
+                chrome_options.add_argument('headless')
+                driver = webdriver.Chrome(options = chrome_options)
+                # Установим time out
+                driver.implicitly_wait(10)
+                driver.get(url)
+                # Ждем загрузки левой панели
+                wait(driver, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR, '._view_full')))
+                # Ищем плашку с координатами
+                driver.find_element(By.CSS_SELECTOR, '.card-title-view')
+                page_source = driver.page_source
+                bsObj = BeautifulSoup(page_source, 'html5lib')
+                coordinats_list = bsObj.find_all('div', {'class' : 'clipboard__action-wrapper _inline'})
+                # Ищем координтаы
+                if len(coordinats_list) > 0:
+                    latitude, longitude = re.findall(r'\d{1,3}.\d{1,10}', coordinats_list[0].text)
+                else:
+                    latitude, longitude = None, None
+                if latitude == city_latitude and longitude == city_longitude:
+                    latitude, longitude = None, None
+                else:
+                    latitude = latitude
+                    longitude = longitude
+                return pd.DataFrame({'address' : address, 'latitude' : [latitude], 'longitude' : [longitude], 'geom' : [None]})
+                break
+            except NoSuchElementException:
+                return pd.DataFrame({'address': address, 'latitude': [None], 'longitude': [None], 'geom': [None]})
+            except TimeoutException:
+                continue
+            finally:
+                driver.close()
